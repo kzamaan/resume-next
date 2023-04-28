8a65d6973c5b7c97ef34bf2507d6bd82f9aae074
Kamruzzaman <kzamanbn@gmail.com>
2023-04-28 16:32:10 +0600

    Update Meta data

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,17 @@ export default function Home() {
         <main className={`${inter.className}`}>
             <Head>
                 <title>KAMRUZZAMAN</title>
+                <meta name='title' property='og:title' content='KAMRUZZAMAN' key='title' />
+                <meta
+                    name='description'
+                    property='og:description'
+                    content='Over two years of experience building web applications, updating existing web applications, and
+                        fixing bugs. I like to learn and use new technology in web development. I have worked
+                        extensively on various stages of application development by creating & implementing application
+                        architecture, which includes various stages in development, code signing, and releasing to
+                        Market and collaborative environments for the web platform.'
+                    key='desc'
+                />
             </Head>
             <Resume />
         </main>
