f6532fdccfb47f9900fb4aa1f07af238a93aa000
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-05-03 21:26:22 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,11 +10,13 @@ extensively on various stages of application development by creating & implement
 architecture, which includes various stages in development, code signing, and releasing to
 Market and collaborative environments for the web platform.`;
 
+const authorName = 'KAMRUZZAMAN (Full Stack)';
+
 export default function App({ Component, pageProps }) {
     return (
         <div className={inter.className}>
             <Head>
-                <title>KAMRUZZAMAN</title>
+                <title>KAMRUZZAMAN (Full Stack)</title>
                 <meta
                     name='viewport'
                     content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
@@ -26,22 +28,22 @@ export default function App({ Component, pageProps }) {
                 <meta name='theme-color' content='#009EF7' />
                 <meta name='apple-mobile-web-app-status-bar' content='#009EF7' />
                 <meta name='apple-mobile-web-app-capable' content='yes' />
-                <meta name='apple-mobile-web-app-title' content='KAMRUZZAMAN' />
-                <meta name='application-name' content='KAMRUZZAMAN' />
+                <meta name='apple-mobile-web-app-title' content={authorName} />
+                <meta name='application-name' content={authorName} />
                 <meta name='msapplication-TileColor' content='#009EF7' />
                 <meta name='msapplication-TileImage' content='/icons/icon-192x192.png' />
                 <meta name='theme-color' content='#009EF7' />
 
                 <meta name='twitter:card' content='summary' />
                 <meta name='twitter:url' content='https://kzaman.vercel.app' />
-                <meta name='twitter:title' content='PWA App' />
+                <meta name='twitter:title' content={authorName} />
                 <meta name='twitter:description' content={description} />
                 <meta name='twitter:image' content='/images/icons/android-chrome-192x192.png' />
                 <meta name='twitter:creator' content='@kzaman' />
-                <meta name='title' property='og:title' content='KAMRUZZAMAN' key='title' />
+                <meta name='title' property='og:title' content={authorName} key='title' />
                 <meta name='description' property='og:description' content={description} key='desc' />
                 <meta property='og:type' content='website' />
-                <meta property='og:site_name' content='KAMRUZZAMAN' />
+                <meta property='og:site_name' content={authorName} />
                 <meta property='og:url' content='https://kzaman.vercel.app' />
                 <meta property='og:image' content='/images/icons/android-chrome-192x192.png' />
             </Head>
